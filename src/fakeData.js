9282db641633cea171b9d258aeaf28957c55d51f
boobaGreen <devboobagreen@gmail.com>
2023-06-05 14:50:24 +0200

    finish

--- src/fakeData.js
+++ src/fakeData.js
@@ -3,7 +3,7 @@ const data = [
     id: 1,
     nome: "Fragolino",
     img: "https://res.cloudinary.com/thomasdea/image/upload/v1612825605/react-api-course/gelateria/gelato-16_bhll5v.jpg",
-    decrizione:
+    descrizione:
       "Lomo pitchfork gentrify poutine craft beer ugh kale chips. Austin bicycle rights tbh disrupt",
     prezzo: 290,
     categoria: "cono",
@@ -12,7 +12,8 @@ const data = [
     id: 2,
     nome: "Testicolo di Orco",
     img: "https://res.cloudinary.com/thomasdea/image/upload/v1612825540/react-api-course/gelateria/gelato-3_nnj0cb.jpg",
-    decrizione: " Skateboard dreamcatcher bicycle rights affogato, YOLO edison",
+    descrizione:
+      " Skateboard dreamcatcher bicycle rights affogato, YOLO edison",
     prezzo: 350,
     categoria: "coppetta",
   },
@@ -20,7 +21,7 @@ const data = [
     id: 6,
     nome: "Banana Joe",
     img: "https://res.cloudinary.com/thomasdea/image/upload/v1612825535/react-api-course/gelateria/gelato-14_ga3apr.jpg",
-    decrizione:
+    descrizione:
       "8-bit hashtag vaporware fixie sartorial normcore meggings pour-over cliche",
     prezzo: 130,
     categoria: "stick",
